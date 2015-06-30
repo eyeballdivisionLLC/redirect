a1d899ea8e2b4ebe0a8a8953f45a0c8557c272a5
Ethan Wolz <ethan@eyeballdivision.com>
2015-06-30 12:30:15 -0600

    redirect to mesh

--- rdr.js
+++ rdr.js
@@ -1,4 +1,6 @@
-var urlParam = getUrlParameter('post');
+
+
+
 
 
 function getUrlParameter(sParam)
@@ -15,6 +17,18 @@ function getUrlParameter(sParam)
     }
 }
 
+
+var aff_id = getUrlParameter('aff_id');
+var aff_sub = getUrlParameter('aff_sub');
+var aff_sub2 = getUrlParameter('aff_sub2');
+var aff_sub3 = getUrlParameter('aff_sub3');
+var aff_sub4 = getUrlParameter('aff_sub4');
+var aff_sub5 = getUrlParameter('aff_sub5');
+var source = getUrlParameter('source');
+var file_id = getUrlParameter('file_id');
+var url_id = getUrlParameter('url_id');
+
+
 $(document).ready(function(){
-        window.location.replace("http://www.mylawfirmfinder.com/" + urlParam);
+        window.location.replace("mesh.mylawfirmfinder.com/v3/c=pinkGreen/?" + 'aff_id=' + aff_id + '&aff_sub=' + aff_sub + '&aff_sub2=' + aff_sub2 + '&aff_sub3=' + aff_sub3 + '&aff_sub4=' + aff_sub4 + '&aff_sub5=' + aff_sub5 + '&source=' + source + '&file_id=' + file_id + '&url_id=' + url_id);
 });
