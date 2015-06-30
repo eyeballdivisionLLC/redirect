3170827c7f93200ff4758aaa96bc26873f788b45
Ethan Wolz <ethan@eyeballdivision.com>
2015-06-30 11:15:08 -0600

    first commit

---
+++ redirect.php
@@ -0,0 +1,20 @@
+    
+<html>
+<head>
+
+    <title>Loading</title>
+
+</head>
+      <body>
+
+
+
+
+
+
+
+
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+              <script src="rdr.js"></script>
+      </body>
+</html>
